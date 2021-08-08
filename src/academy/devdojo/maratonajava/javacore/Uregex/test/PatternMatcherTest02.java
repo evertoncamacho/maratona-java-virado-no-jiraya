b89ddefb324e5c67d17286b4310d2824568b25e9
Everton Camacho <everton.camacho@hotmail.com>
2021-08-07 21:26:21 -0300

    Class 132

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest02 {
+
+    public static void main(String[] args) {
+
+        // \d = Todos os dígitos
+        // \D = Tudo o que não for dígitos
+        // \s = Todos os espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-ZA-Z, dígitos, _
+        // \W = Tudo o que não for incluso no \w
+
+        String regex = "\\W";
+        //String texto = "abaaba";
+        String texto2 = "@#$hh_q2 34gfk17";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto2);
+        System.out.println("texto:  " + texto2);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex: " + regex);
+        System.out.println("Posições encontradas");
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+
+    }
+}
